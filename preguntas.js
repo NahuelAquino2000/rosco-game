a03dc12b82eee3322a69104c6dd9ccd5cb651999
naquino <nahuelaquino.a@gmail.com>
2026-02-21 02:01:44 +0000

    test

---
+++ preguntas.js
@@ -0,0 +1,7 @@
+const roscoData = [
+    { letra: "A", respuesta: "Astro", definicion: "Empieza con A: Cuerpo celeste que puebla el universo." },
+    { letra: "B", respuesta: "Barco", definicion: "Empieza con B: Vehículo flotante que se utiliza para transportarse por el agua." },
+    { letra: "C", respuesta: "Cielo", definicion: "Empieza con C: Espacio en el que se mueven los astros y que por efecto visual parece rodear la Tierra." },
+    // ... Agrega el resto del abecedario hasta la Z siguiendo este mismo formato
+    { letra: "Z", respuesta: "Zoológico", definicion: "Empieza con Z: Lugar donde se cuidan y exhiben animales salvajes." }
+];
